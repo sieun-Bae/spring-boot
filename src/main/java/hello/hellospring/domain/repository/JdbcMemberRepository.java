bb030e0cf126ff64c49abf7c5a0b655576f98af1
sieun-Bae <qotldms130@gmail.com>
2021-06-04 07:10:36 +0900

    pure jdbc..

---
+++ src/main/java/hello/hellospring/domain/repository/JdbcMemberRepository.java
@@ -0,0 +1,72 @@
+package hello.hellospring.domain.repository;
+
+import hello.hellospring.domain.Member;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.List;
+import java.util.Optional;
+
+import static jdk.internal.net.http.common.Utils.close;
+import static org.springframework.jdbc.datasource.DataSourceUtils.getConnection;
+
+public class JdbcMemberRepository implements MemberRepository {
+
+    private final DataSource dataSource;
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(name) values(?)";
+        /*
+        Connection connection = dataSource.getConnection();
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        pstmt.setString(1, member.getName());
+
+        pstmt.executeUpdate();
+
+        return null;
+
+        */
+        String sql = "insert into member(name) values(?)";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, member.getName());
+            pstmt.executeUpdate();
+            rs = pstmt.getGeneratedKeys();
+            if (rs.next()) {
+                member.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+            return member;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
