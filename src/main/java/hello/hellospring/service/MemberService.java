f6a73b7ae4d004f89f2e80a41f6e930f0fcb6e3d
sieun-Bae <qotldms130@gmail.com>
2021-06-04 05:18:29 +0900

    Service, test Unit, dependency injection

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,49 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.domain.repository.MemberRepository;
+import hello.hellospring.domain.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    //Dependency Injection!
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //같은 이름이 있는 회원은 안된다.
+    private void validateDuplicateMember(Member member) {
+        /* Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }); */
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
